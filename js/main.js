4e3ce0626fda73b377e583b5e4b7f30f2133029f
Sean McPherson <seanmcp25@gmail.com>
2017-09-09 17:12:11 -0400

    Convert weights to lbs; add bootstrap

--- js/main.js
+++ js/main.js
@@ -3,25 +3,42 @@ let searchButton = document.getElementById('submitButton')
 
 let searchForm = document.getElementById('searchForm')
 
-searchButton.addEventListener('click', function() {
-
+let getInfo = function() {
   outputParent.innerHTML = ''
+
+  let weightLbs = 0;
+
+  let age = document.getElementById('searchAge').value
+  let weight = document.getElementById('searchWeight').value
+  let unit = document.getElementById('searchUnit').value
+
+  if(unit === 'kg'){
+    weightLbs = weight * 2.20462
+  } else if(unit === 'stone') {
+    weightLbs = weight * 14
+  } else {
+    weightLbs = weight
+  }
 
   let req = {
     body: {
-      age: document.getElementById('searchAge').value,
-      weight: document.getElementById('searchWeight').value,
-      unit: document.getElementById('searchUnit').value
+      age: age,
+      weightLbs: weightLbs
     }
   }
 
-  if(!req.body.age || !req.body.weight) {
+  if(!req.body.age || !req.body.weightLbs) {
     let para = document.createElement('p')
     para.textContent = 'Please enter age and weight'
-    para.className = "error"
+    para.className = 'error'
     outputParent.appendChild(para)
-
   } else {
-    console.log('req', req);
+    let para = document.createElement('p')
+    para.textContent = `Your child is ${age} and weighs ${weight} ${unit}`
+    para.className = 'error'
+    outputParent.appendChild(para)
   }
-})
+}
+
+searchButton.addEventListener('click', getInfo)
+searchForm.addEventListener('submit', getInfo)
